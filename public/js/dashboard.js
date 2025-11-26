4b2683a7c3929fa0dce1b2368994a78d2f7b55ac
LodosLawson <lodoslawson@gmail.com>
2025-11-26 19:47:06 +0100

    fix: Make API base URL dynamic for production deployment

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,8 @@
 // Dashboard functionality
-const API_BASE = 'http://localhost:3000';
+// Use current host if deployed, otherwise localhost
+const API_BASE = window.location.hostname === 'localhost'
+    ? 'http://localhost:3000'
+    : window.location.origin;
 
 async function fetchStatus() {
     try {
