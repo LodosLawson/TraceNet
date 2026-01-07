875f7f066aa1e5d3c26a800d8fae39a2d6fa6f57
LodosLawson <lodoslawson@gmail.com>
2026-01-07 23:38:44 +0100

    Fix: Force any type for atmosphereMaterial to resolve build error

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,11 +1,7 @@
-/// <reference types="vite/client" />
-import { Object3DNode } from '@react-three/fiber';
-import { ShaderMaterial } from 'three';
-
 declare global {
     namespace JSX {
         interface IntrinsicElements {
-            atmosphereMaterial: Object3DNode<ShaderMaterial, typeof ShaderMaterial>;
+            atmosphereMaterial: any;
         }
     }
 }
